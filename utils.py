747433ba8440d4dcf6a6559bf3a88034a7d3e3ed
park4134 <jasonpcy0121@gmail.com>
2024-09-25 16:53:46 +0900

    Init | model & loss code

---
+++ utils.py
@@ -0,0 +1,12 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+def loss_function(x_recon, x, mu, sigma):
+    # 복원 오차 (MSE)
+    recon_loss = nn.functional.mse_loss(x_recon, x, reduction='sum')
+    
+    # KLD (Kullback-Leibler Divergence)
+    kld_loss = -0.5 * torch.sum(1 + sigma - mu.pow(2) - sigma.exp())
+    
+    return recon_loss + kld_loss
